4ce90f227b58c074b7425c98414fadf49b33df1f
Kamikaze <codewithkamikaze@gmail.com>
2026-03-15 21:28:52 +0300

    Add todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TodoList from "./components/TodoList";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { TodosContext } from "./contexts/todosContext";
+import { useState } from "react";
+
+import { v4 as uuidv4 } from "uuid";
+import MySnackBar from "./components/MySnackBar";
+import TodosProvider from "./contexts/todosContext";
+import { ToastProvider } from "./contexts/ToastContext";
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Alexandria"],
+  },
+
+  palette: {
+    primary: {
+      main: "#dd2c00",
+    },
+  },
+});
+
+const initialTodos = [
+  {
+    id: uuidv4(),
+    title: "قراءة كتاب",
+    details: "تيسمبتيس يتسبميتس بيمستب",
+    isCompleted: false,
+  },
+  {
+    id: uuidv4(),
+    title: "قراءة كتاب",
+    details: "تيسمبتيس يتسبميتس بيمستب",
+    isCompleted: false,
+  },
+  {
+    id: uuidv4(),
+    title: "قراءة كتاب",
+    details: "تيسمبتيس يتسبميتس بيمستب",
+    isCompleted: false,
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <TodosProvider>
+        <ToastProvider>
+          <div
+            className="App"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              background: "#191b1f",
+              height: "100vh",
+              direction: "rtl",
+            }}
+          >
+            <TodoList />
+          </div>
+        </ToastProvider>
+      </TodosProvider>
+    </ThemeProvider>
   );
 }
 
